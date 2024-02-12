57f0ae001ceb2b745c04ec9efccd7816016851e5
Amalprneopraxis <amal.pr@neopraxis.in>
2024-02-12 18:10:44 +0530

    change the export type

--- src/Headernav.js
+++ src/Headernav.js
@@ -1,11 +1,13 @@
 
 
-export default function Headernav(){
-    return(
-        <>
-        <div>
-            Navigation component is coming
-        </div>
-        </>
-    );
-}
+import React from 'react';
+
+const Headernav = () => {
+  return (
+    <div>
+      <h1>Header component is available now</h1>
+    </div>
+  );
+};
+
+export default Headernav;
