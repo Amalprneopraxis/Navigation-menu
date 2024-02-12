14e86fdacf23c54e1245f2977a021f00eacec3f3
Amalprneopraxis <amal.pr@neopraxis.in>
2024-02-12 16:29:47 +0530

    added webpack file for cdn

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './src/Headernav.js', // Path to your component entry file
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: 'Headernav.bundle.js', // Output filename
+    library: 'Headernav', // Name of the library
+    libraryTarget: 'umd', // Universal Module Definition
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+};
