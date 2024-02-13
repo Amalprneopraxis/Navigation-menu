9e21e494e66012b1cecd2bf81290833046a77b0a
Amalprneopraxis <amal.pr@neopraxis.in>
2024-02-13 18:00:51 +0530

    add a retun component

--- src/Headernav.js
+++ src/Headernav.js
@@ -5,9 +5,15 @@ import React from 'react';
 const Headernav = () => {
   return (
     <div>
-      <h1>Header component is available now</h1>
+      <h1>Name: John Doe</h1>
+      <p>Age: 30</p>
+      <p>City: New York</p>
     </div>
   );
 };
 
-export default Headernav;
+
+const Navigationmenu = () => {
+  return <Headernav />;
+};
+export default Navigationmenu;
